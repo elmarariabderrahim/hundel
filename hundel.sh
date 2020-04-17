7a2ad770d1df3f3f2d4e2a4382c9f6064d769f41
elmarariabderrahim <32686758+elmarariabderrahim@users.noreply.github.com>
2020-04-17 14:24:11 +0100

    Update hundel.sh

--- hundel.sh
+++ hundel.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 
 
-JOB_NAME=$1
+# JOB_NAME=$1
 # WORKSPACE=$2
 # VERSION_NUMBER=$3
 # PLATEFORME_SOURCE=$4
@@ -14,32 +14,32 @@ JOB_NAME=$1
 # SCRIPT_BASEDIR_PATH=$(dirname "$SCRIPT_PATH")
 # . ${SCRIPT_BASEDIR_PATH}/environment_config.sh
 
-# THERE_WERE_REJECTED_FILES=0
-# SCRIPT_HANDLED_LIST=()
-# Read_DB_Name() {
-# 	DB_INSTRUCTION="NODBINSTRUCTIONINTHEFILE"
-# 	while read -r line
-# 	do
-#     	TEMP=$(echo $line | tr '[:lower:]' '[:upper:]')
-#     	#echo "line $TEMP"
-#     	if [[ $TEMP = *"USE"* ]]; then
-#     		if [[ $2 -eq 1 ]]; then
-#     			DB_INSTRUCTION=$TEMP
-#     		else 
-#     			DB_INSTRUCTION=$line
-#     		fi
-#     		break
-#     	fi
-# 	done < "$1"
-# 	RESULT=""
-# 	if [[ $DB_INSTRUCTION = *"\`"* ]] 
-# 		then 
-# 		RESULT=$(echo $DB_INSTRUCTION | cut -d"\`" -f2 | xargs)
-# 	else
-# 		RESULT=$(echo $DB_INSTRUCTION | cut -d" " -f2 | cut -d";" -f1 | xargs)
-# 	fi
-#   	echo -e "${RESULT}"
-# }
+THERE_WERE_REJECTED_FILES=0
+SCRIPT_HANDLED_LIST=()
+Read_DB_Name() {
+	DB_INSTRUCTION="NODBINSTRUCTIONINTHEFILE"
+	while read -r line
+	do
+    	TEMP=$(echo $line | tr '[:lower:]' '[:upper:]')
+    	#echo "line $TEMP"
+    	if [[ $TEMP = *"USE"* ]]; then
+    		if [[ $2 -eq 1 ]]; then
+    			DB_INSTRUCTION=$TEMP
+    		else 
+    			DB_INSTRUCTION=$line
+    		fi
+    		break
+    	fi
+	done < "$1"
+	RESULT=""
+	if [[ $DB_INSTRUCTION = *"\`"* ]] 
+		then 
+		RESULT=$(echo $DB_INSTRUCTION | cut -d"\`" -f2 | xargs)
+	else
+		RESULT=$(echo $DB_INSTRUCTION | cut -d" " -f2 | cut -d";" -f1 | xargs)
+	fi
+  	echo -e "${RESULT}"
+}
 for inode in `ls -1 sql_scripts`
 	do
   DB_NAME_IN_SCRIPT_UPPERCASE=`Read_DB_Name $inode 1`
