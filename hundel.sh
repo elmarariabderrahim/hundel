7b9ebec4bc5a7c0c3f2d53e942c2f76a72e05b4f
elmarariabderrahim <32686758+elmarariabderrahim@users.noreply.github.com>
2020-04-17 13:35:07 +0100

    Update hundel.sh

--- hundel.sh
+++ hundel.sh
@@ -1,6 +1,48 @@
 #!/bin/bash
-j=$1
-path=$(pwd)
-echo $path
-echo $j
+
+
+JOB_NAME=$1
+# WORKSPACE=$2
+# VERSION_NUMBER=$3
+# PLATEFORME_SOURCE=$4
+# VERSION_NAME="V$3"
+# PLATEFORME=`echo $JOB_NAME |cut -d"_" -f1`
+# #BRANCHE=`echo $JOB_NAME |cut -d"_" -f2`
+# # lecture des parametres / variables environnements
+# SCRIPT_NAME=`basename "$0"`
+# SCRIPT_PATH=$(readlink -f "$0")
+# SCRIPT_BASEDIR_PATH=$(dirname "$SCRIPT_PATH")
+# . ${SCRIPT_BASEDIR_PATH}/environment_config.sh
+
+# THERE_WERE_REJECTED_FILES=0
+# SCRIPT_HANDLED_LIST=()
+# Read_DB_Name() {
+# 	DB_INSTRUCTION="NODBINSTRUCTIONINTHEFILE"
+# 	while read -r line
+# 	do
+#     	TEMP=$(echo $line | tr '[:lower:]' '[:upper:]')
+#     	#echo "line $TEMP"
+#     	if [[ $TEMP = *"USE"* ]]; then
+#     		if [[ $2 -eq 1 ]]; then
+#     			DB_INSTRUCTION=$TEMP
+#     		else 
+#     			DB_INSTRUCTION=$line
+#     		fi
+#     		break
+#     	fi
+# 	done < "$1"
+# 	RESULT=""
+# 	if [[ $DB_INSTRUCTION = *"\`"* ]] 
+# 		then 
+# 		RESULT=$(echo $DB_INSTRUCTION | cut -d"\`" -f2 | xargs)
+# 	else
+# 		RESULT=$(echo $DB_INSTRUCTION | cut -d" " -f2 | cut -d";" -f1 | xargs)
+# 	fi
+#   	echo -e "${RESULT}"
+# }
+for inode in `ls -1 $1`
+	do
+  DB_NAME_IN_SCRIPT_UPPERCASE=`Read_DB_Name $1/$inode 1`
+	DB_NAME_IN_SCRIPT_LOWERCASE=`Read_DB_Name $1/$inode 2`
+done
 
