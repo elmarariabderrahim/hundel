0137b106ec286069260a02fdfd80df24a0642934
elmarariabderrahim <32686758+elmarariabderrahim@users.noreply.github.com>
2020-04-20 12:04:42 +0000

    Update hundel.sh

--- hundel.sh
+++ hundel.sh
@@ -1,21 +1,7 @@
-#!/bin/bash
-
-
-JOB_NAME=$1
-WORKSPACE=$2
-VERSION_NUMBER=$3
-PLATEFORME_SOURCE=$4
-VERSION_NAME="V$3"
-PLATEFORME=`echo $JOB_NAME |cut -d"_" -f1`
-# #BRANCHE=`echo $JOB_NAME |cut -d"_" -f2`
-# # lecture des parametres / variables environnements
-# SCRIPT_NAME=`basename "$0"`
-# SCRIPT_PATH=$(readlink -f "$0")
-# SCRIPT_BASEDIR_PATH=$(dirname "$SCRIPT_PATH")
-# . ${SCRIPT_BASEDIR_PATH}/environment_config.sh
-
-THERE_WERE_REJECTED_FILES=0
-SCRIPT_HANDLED_LIST=()
+export username=$1
+export password=$2
+list_database_in_script=()
+flag=""
 Read_DB_Name() {
 	DB_INSTRUCTION="NODBINSTRUCTIONINTHEFILE"
 	while read -r line
@@ -23,12 +9,12 @@ Read_DB_Name() {
     	TEMP=$(echo $line | tr '[:lower:]' '[:upper:]')
     	#echo "line $TEMP"
     	if [[ $TEMP = *"USE"* ]]; then
-    		if [[ $2 -eq 1 ]]; then
+    		
     			DB_INSTRUCTION=$TEMP
-    		else 
+    		 
     			DB_INSTRUCTION=$line
-    		fi
-    		break
+    		
+    		
     	fi
 	done < "$1"
 	RESULT=""
@@ -41,18 +27,34 @@ Read_DB_Name() {
   	echo -e "${RESULT}"
 }
 
+for f in PATT_UTILS/sql_scripts/*; do
+script_name=$(echo $f| cut -d'/' -f 2)
+DB_NAME_IN_SCRIPT_UPPERCASE=`Read_DB_Name $f `
+echo $DB_NAME_IN_SCRIPT_UPPERCASE
+	if [[  $DB_NAME_IN_SCRIPT_UPPERCASE != "NODBINSTRUCTIONINTHEFILE" ]]; then 
 
-Handle_Directory_Script() {
-SCRIPT_NOT_HANDLED_LIST=()
-for inode in sql_scripts/*; do
-	GREP_INODE_REGISTER_FILE=`grep "$inode" $REGISTER_FILE | tail -1` 
-	CHECKSUM_FOUND_IN_REGISTER_FILE=`echo $GREP_INODE_REGISTER_FILE | cut -d"|" -f2`
-	CHECKSUM_VALUE=`md5sum $inode | awk '{print $1}'`
-	echo "New checksum $CHECKSUM_VALUE ; Existant checksum $CHECKSUM_FOUND_IN_REGISTER_FILE"
+		if [[ ! ${list_database_in_script[*]} =~ "$DB_NAME_IN_SCRIPT_UPPERCASE" ]]; then 
+			list_database_in_script+=("$DB_NAME_IN_SCRIPT_UPPERCASE")
+		else 
+			echo "$d est deja exporter "
+		fi
+	else
+		echo "la base de donnee n'est pas specifier dans le script $script_name "
+	fi
+done	
+for d in ${list_database_in_script[@]}; do
+	echo " +++++"+$d
 done
 
 
-}
-echo $DB_NAME_IN_SCRIPT_LOWERCASE
-echo $DB_NAME_IN_SCRIPT_UPPERCASE
+str=$(docker port test-mysql)
+IFS=':'
+read -ra ADDR <<< "$str"
+docker_mysql_port=${ADDR[1]}
 
+for d in ${list_database_in_script[@]}; do
+	echo " ---"+$d
+	mysqldump -u $username -p$password --no-data $d > $d.sql
+	mysql -P $docker_mysql_port --protocol=tcp -u $username -p$password -Bse "CREATE DATABASE IF NOT EXISTS $d; "
+	docker exec -i dadbc6ef4b91  mysql -u $username -p$password  $d < $d.sql
+done
