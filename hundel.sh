fc74d27873f99b866257fc87809c54c30a811d16
elmarariabderrahim <32686758+elmarariabderrahim@users.noreply.github.com>
2020-04-21 00:06:57 +0000

    Update hundel.sh

--- hundel.sh
+++ hundel.sh
@@ -1,3 +1,5 @@
+
+#!/bin/bash
 export username=$1
 export password=$2
 list_database_in_script=()
@@ -27,26 +29,38 @@ Read_DB_Name() {
   	echo -e "${RESULT}"
 }
 
-for f in PATT_UTILS/sql_scripts/*; do
+
+for f in sql_script/*; do
 script_name=$(echo $f| cut -d'/' -f 2)
 DB_NAME_IN_SCRIPT_UPPERCASE=`Read_DB_Name $f `
-echo $DB_NAME_IN_SCRIPT_UPPERCASE
-	if [[  $DB_NAME_IN_SCRIPT_UPPERCASE != "NODBINSTRUCTIONINTHEFILE" ]]; then 
+count=${#list_database_in_script[@]}
+flag1=""
+for (( c=0; c<$count; c++ ))
+do 
+	
+				if [[  ${list_database_in_script[$c]} = ${DB_NAME_IN_SCRIPT_UPPERCASE} ]]; then 
+					flag1=1
+				else 
+					flag1=0
+				fi
 
-		if [[ ! ${list_database_in_script[*]} =~ "$DB_NAME_IN_SCRIPT_UPPERCASE" ]]; then 
-			list_database_in_script+=("$DB_NAME_IN_SCRIPT_UPPERCASE")
-		else 
-			echo "$d est deja exporter "
-		fi
+done
+
+	if [[  $DB_NAME_IN_SCRIPT_UPPERCASE != "NODBINSTRUCTIONINTHEFILE" ]]; then 
+	 	if [[  $flag1 != 1 ]]; then 
+						list_database_in_script+=($DB_NAME_IN_SCRIPT_UPPERCASE)
+					
+				fi
+
 	else
 		echo "la base de donnee n'est pas specifier dans le script $script_name "
 	fi
-done	
-for d in ${list_database_in_script[@]}; do
-	echo " +++++"+$d
 done
+count=${#list_database_in_script[@]}
+echo "nombre des bases invoquer dans les scripts est" $count	
 
 
+ # inportation des bases invoquer dans les scripts
 str=$(docker port test-mysql)
 IFS=':'
 read -ra ADDR <<< "$str"
@@ -55,6 +69,12 @@ docker_mysql_port=${ADDR[1]}
 for d in ${list_database_in_script[@]}; do
 	echo " ---"+$d
 	mysqldump -u $username -p$password --no-data $d > $d.sql
-	mysql -P $docker_mysql_port --protocol=tcp -u $username -p$password -Bse "CREATE DATABASE IF NOT EXISTS $d; "
+	mysql -P $docker_mysql_port --protocol=tcp -u $username -p$password -Bse "DROP DATABASE IF EXISTS $d; CREATE DATABASE  $d; "
 	docker exec -i dadbc6ef4b91  mysql -u $username -p$password  $d < $d.sql
 done
+
+
+
+
+
+
